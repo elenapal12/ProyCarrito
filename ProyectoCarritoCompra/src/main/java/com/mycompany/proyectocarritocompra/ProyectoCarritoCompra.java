66255b4a1815930cae2dce6d109d4225f3d337dc
Elena <andpalacios39@gmail.com>
2025-06-27 18:04:51 -0400

    quinto commit

--- ProyectoCarritoCompra/src/main/java/com/mycompany/proyectocarritocompra/ProyectoCarritoCompra.java
+++ ProyectoCarritoCompra/src/main/java/com/mycompany/proyectocarritocompra/ProyectoCarritoCompra.java
@@ -6,11 +6,40 @@ package com.mycompany.proyectocarritocompra;
 
 /**
  *
- * @author andre
+ * @author andrea/cecilia
  */
 public class ProyectoCarritoCompra {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo 3");
+        Scanner sc = new Scanner(System.in);
+        Carrito carrito = new Carrito();
+
+        // Catálogo simple
+        Producto[] catalogo = {
+            new Producto("Pan", 2.0),
+            new Producto("Leche", 5.5),
+            new Producto("Arroz", 4.2),
+            new Producto("Huevos", 7.0)
+        };
+
+        int opcion;
+        do {
+            System.out.println("\n=== Menú de Compras ===");
+            System.out.println("1. Ver catálogo");
+            System.out.println("2. Agregar producto al carrito");
+            System.out.println("3. Ver carrito");
+            System.out.println("4. Ver total");
+            System.out.println("5. Salir");
+            System.out.print("Opción: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Catálogo disponible:");
+                    for (int i = 0; i < catalogo.length; i++) {
+                        System.out.println((i + 1) + ". " + catalogo[i]);
+                    }
+                    break;
+
     }
 }
