c1556f80ac94e1d25a159a2786ccd41fb5d60724
Elena <andpalacios39@gmail.com>
2025-06-27 17:22:36 -0400

    primer commit

---
+++ ProyectoCarritoCompra/src/main/java/com/mycompany/proyectocarritocompra/ProyectoCarritoCompra.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectocarritocompra;
+
+/**
+ *
+ * @author andre
+ */
+public class ProyectoCarritoCompra {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
